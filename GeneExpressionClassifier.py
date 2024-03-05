01fea9fc171b3dffad8e5efa430750494daf6863
Tom Alisch <alisch.tom@web.de>
2024-03-05 16:48:26 -0500

    Renamed main .py file

---
+++ GeneExpressionClassifier.py
@@ -0,0 +1,11 @@
+## Using public labeled gene expression data to predict cancer type using different models
+# 1) Load and clean data
+# 2) Balance groups and create data loaders
+# 3) Build, train, validate, and test accuracy across different ML models
+# 4) Print output and conclude
+
+## Dependencies
+import pydna
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
